b24bc1cc2cea24e59763aa78bb9e9e76b7544483
dayan2 <dayanmendis2@gmail.com>
2015-02-23 22:32:24 +0530

    ajax_Notification

---
+++ Axaj_Add_Confirmation_jquery/Axaj_Add_Confirmation_jquery/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Axaj_Add_Confirmation_jquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(true);
+        }
+        [HttpPost]
+        public ActionResult Index(string str)
+        {
+            bool res = true;
+            if (str == "Jude")
+                res = false;
+            return Json(res);
+        }
+
+    }
+}
